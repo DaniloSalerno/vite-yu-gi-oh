3002edb003cf2731ea542b77fc712be91b8ed29e
DaniloSalerno <salerno.danilo@hotmail.it>
2023-09-26 16:44:22 +0200

    add v-for in AppCard component to have dinamic card and add card style

--- src/state.js
+++ src/state.js
@@ -3,18 +3,14 @@ import axios from 'axios';
 
 export const state = reactive({
     url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
-    archetype: null,
-    cardName: null,
-    cardUrl: null,
+    cards: null,
     fetchDataCard() {
 
         axios
             .get(this.url)
             .then(response => {
 
-                this.cardName = response.data.data[0].name;
-                this.cardUrl = response.data.data[0].card_images[0].image_url;
-                this.archetype = response.data.data[0].archetype;
+                this.cards = response.data.data
 
             })
             .catch(error => {
