eab1ed4c8af77f11ee29c328c94d64535a029828
DaniloSalerno <salerno.danilo@hotmail.it>
2023-09-26 14:55:43 +0200

    fetch data by API with axios and console.log of names of cards

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,30 @@
 <script >
+
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+    }
+  },
+  created() {
+
+    for (let i = 0; i < 20; i++) {
+      axios
+        .get(this.url)
+        .then(response => {
+          console.log(response.data.data[i].name);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+
+    }
+  }
+}
+
 </script>
 
 <template></template>
