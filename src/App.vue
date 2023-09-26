92171c90e4561960013ea3975a149d4290c886c0
DaniloSalerno <salerno.danilo@hotmail.it>
2023-09-26 15:06:50 +0200

    add AppHeader component
    contains logo and title

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script >
 
 import axios from 'axios';
-
+import AppHeader from './components/AppHeader.vue'
 export default {
   name: 'App',
   data() {
     return {
       url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
     }
+  },
+  components: {
+    AppHeader
   },
   created() {
 
@@ -27,6 +30,8 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+</template>
 
 <style scoped></style>
